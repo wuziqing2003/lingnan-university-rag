062564713bf7701bcee92b39a54870f5c0857c84
Fish <19047117860@163.com>
2026-07-11 11:28:51 +0800

    refactor: restrict api outputs using protective response models

--- Fastapi_practice.py
+++ Fastapi_practice.py
@@ -5,24 +5,42 @@ from sqlalchemy.orm import Session
 import uvicorn
 from pydantic import BaseModel,Field
 from typing import Annotated
+from datetime import datetime
+from fastapi import HTTPException
 Base.metadata.create_all(bind=engine)
 
 class UserCreateSchema(BaseModel):
     username : Annotated[str,Field(min_length = 3 ,max_length = 20)]
     password : Annotated[str,Field(min_length = 6 ,max_length = 30)]
 
+class UserResponseSchema(BaseModel):
+    id : int
+    username : str
+    created_at : datetime
+    class Config:
+        from_attributes = True
+
+
 
 app = FastAPI(title = "lingnang-api")
 
 
-@app.post("/user")
+@app.post("/user",response_model=UserResponseSchema)
 async def creat_user(user :UserCreateSchema,db : Session = Depends(get_db)):
     db_user = User(username=user.username,password=user.password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return {"message":"用户注册成功！","user":{"user_id":db_user.id,"user_username":db_user.username}}
-    
+    return db_user
+@app.get("/user/{id}",response_model=UserResponseSchema)
+async def get_user_by_id(id : int ,db : Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="用户不存在")
+    return user
+
+
+
 
 @app.get("/user")
 async def get_user(page: int = Query(1,ge=1),size : int = Query(10,ge=10,le=100),db :Session = Depends(get_db)):
--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = "users"
     id  =  Column(Integer,primary_key=True,autoincrement=True,comment="用户ID")
     username = Column(String(50),unique=True,nullable=False,comment="用户名")
-    password = Column(String(255),nullable=False,comment="用户密码")
+    password_hash = Column(String(255),nullable=False,comment="密码哈希")
     created_at = Column(DateTime,default=func.now(),comment="创建时间")
 
 
