a96cc6c5fd69ab597243019688279e86aefb72f9
Fish <19047117860@163.com>
2026-07-18 13:28:49 +0800

    chore: remove unused streamlit import from rag chain

--- app/rag/chain.py
+++ app/rag/chain.py
@@ -1,11 +1,14 @@
-import chromadb
-from openai import OpenAI
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough,RunnableLambda
 from langchain_openai import ChatOpenAI
-from streamlit.cursor import make_delta_path
+from openai import OpenAI
 from app.core.config import SiliconFlow_API_KEY,DEEPSEEK_API_KEY
+import chromadb
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableLambda,RunnablePassthrough
+
+
+
+
 
 
 embed_client = OpenAI(
@@ -14,48 +17,47 @@ embed_client = OpenAI(
 )
 
 
-def get_embedding(text):
+def get_embedding(text:str):
     resp = embed_client.embeddings.create(
-        model = "BAAI/bge-large-zh-v1.5",
+        model =  "BAAI/bge-large-zh-v1.5",
         input = text,
     )
     return resp.data[0].embedding
 
-chroma =  chromadb.PersistentClient(path="./chroma_db")
+chroma = chromadb.PersistentClient(path="./chroma_db")
 collection = chroma.get_or_create_collection("lingnan_rag")
 
 def retrieve(question):
-    results = collection.query(
+    result = collection.query(
         query_embeddings=[get_embedding(question)],
-        n_results=3,
+        n_results=3
     )
-    docs = results["documents"][0] or []
+    docs = result["documents"][0] or []
 
-    return "\n\n".join(docs)
+    return "/n/n".join(docs)
 
 prompt = ChatPromptTemplate([
-    ("system", "你只能根据给定资料回答，资料没有的内容就说不知道。"),
-    ("human", "根据已知信息: {context}, 请回答问题: {question}"),
+    ("system","你只能根据给定资料回答，资料没有的内容就说不知道。"),
+    ("human","根据已知信息{context},请回答问题{question}"),
 ])
 
 model = ChatOpenAI(
-    model="deepseek-v4-pro",
-    api_key=DEEPSEEK_API_KEY,
-    base_url="https://api.deepseek.com",
+    model = "deepseek-v4-pro",
+    base_url = "https://api.deepseek.com",
+    api_key = DEEPSEEK_API_KEY,
     temperature=0,
-    streaming=True,  # 流式建议打开
+    streaming=True,
 )
-
 
 parser = StrOutputParser()
 
-rag_chain = (
-    {
-    "context":RunnableLambda(retrieve),
-    "question":RunnablePassthrough(),
-    }
-    | prompt
-    | model
-    | parser
 
+rag_chain = (
+   {
+    "context":RunnableLambda(retrieve),
+    "question":RunnablePassthrough()
+   }
+    |prompt
+    |model
+    |parser
 )
